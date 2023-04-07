6ae448b32305ce916d8c2ccb520d2a614c7a75cd
tot0p <thomas.lemaitre.78420@gmail.com>
2023-04-07 10:59:14 +0200

    update

--- client.py
+++ client.py
@@ -54,110 +54,13 @@ class Client(commands.Bot):
         return await super().clear()
     
     def registerCommand(self):
-
         load_commands(COMMANDS_MESSAGE_PREFIX_DIR,{"client":self,"OWNER_ID":OWNER_ID})
-
-
-        # @self.command()
-        # async def add(ctx,data):
-        #     if self.historic == None:
-        #         self.historic = ListChained(data)
-        #     else:
-        #         self.historic.append(data)
-        #     await ctx.send(self.historic)
-
-        # @self.command()
-        # async def insert(ctx,indice,data):
-        #     if self.historic == None:
-        #         self.historic = ListChained(data)
-        #     else:
-        #         self.historic.insert(indice,data)
-        #     await ctx.send(self.historic)
-
-        # @self.command()
-        # async def insert_first(ctx,data):
-        #     if self.historic == None:
-        #         self.historic = ListChained(data)
-        #     else:
-        #         self.historic.insert_first(data)
-        #     await ctx.send(self.historic)        
-
-        # @self.command()
-        # async def ping(ctx):
-        #     await ctx.send('Pong!')
-
-        # @self.command()
-        # async def delete(ctx, amount=5):
-        #     await ctx.channel.purge(limit=amount)
     
-
-
     def registerSlashCommand(self):
-        
-
         load_commands(COMMANDS_SLASH_DIR,{"client":self,"OWNER_ID":OWNER_ID})
 
-        # @self.tree.command()
-        # async def ping(interaction: discord.Interaction):
-        #     """Pong!"""
-        #     await interaction.response.send_message('Pong!', ephemeral=True)
-
-
-        # @self.tree.command()
-        # async def say(interaction: discord.Interaction, *, message: str):
-        #     """Make the bot say something"""
-        #     await interaction.response.send_message(message, ephemeral=True)
-
-        # @self.tree.command()
-        # async def turnoff(interaction: discord.Interaction):
-        #     """Turn off the bot"""
-        #     if interaction.user.id != OWNER_ID:
-        #         return await interaction.response.send_message('You are not the owner of this bot.', ephemeral=True)
-        #     await interaction.response.send_message('Turning off...', ephemeral=True)
-        #     await self.close()
-
-        # @self.tree.command()
-        # @app_commands.describe(name='The name of role to create')
-        # async def create_role(interaction: discord.Interaction, name: str):
-        #     """Create a role"""
-        #     guild = interaction.guild
-        #     if discord.utils.get(guild.roles, name=name) is not None:
-        #         return await interaction.response.send_message('That role already exists.', ephemeral=True)
-        #     await guild.create_role(name=name)
-        #     await interaction.response.send_message(f'Created the {name} role.', ephemeral=True)
-
-
-        # @self.tree.command()
-        # @app_commands.describe(name='The name of role to add')
-        # async def add_role(interaction: discord.Interaction, name: str):
-        #     """Add a role to the user"""
-        #     guild = interaction.guild
-        #     role = discord.utils.get(guild.roles, name=name)
-        #     if role is None:
-        #         return await interaction.response.send_message('That role does not exist.', ephemeral=True)
-        #     await interaction.user.add_roles(role)
-        #     await interaction.response.send_message(f'Added the {role.name} role to you.', ephemeral=True)
-
-        # @self.tree.command()
-        # @app_commands.describe(name='The name of role to remove')
-        # async def remove_role(interaction: discord.Interaction, name: str):
-        #     """Remove a role from the user"""
-        #     guild = interaction.guild
-        #     role = discord.utils.get(guild.roles, name=name)
-        #     if role is None:
-        #         return await interaction.response.send_message('That role does not exist.', ephemeral=True)
-        #     await interaction.user.remove_roles(role)
-        #     await interaction.response.send_message(f'Removed the {role.name} role from you.', ephemeral=True)
-
-
-
     def registerContextMenu(self):
-        @self.tree.context_menu(name="Turn Off")
-        async def turnoff(interaction : discord.Interaction, user : discord.Member):
-            if interaction.user.id != OWNER_ID:
-                return await interaction.response.send_message('You are not the owner of this bot.', ephemeral=True)
-            await interaction.response.send_message('Turning off...', ephemeral=True)
-            await self.close()
+        load_commands(COMMANDS_CONTEXT_MENU_DIR,{"client":self,"OWNER_ID":OWNER_ID})
 
 
 def load_commands(DIR: str,ENV : dict):
---
+++ commands/context_menu/turnoff.py
@@ -0,0 +1,9 @@
+import discord
+
+
+@client.tree.context_menu(name="Turn Off")
+async def turnoff(interaction : discord.Interaction, user : discord.Member):
+    if interaction.user.id != OWNER_ID:
+        return await interaction.response.send_message('You are not the owner of this bot.', ephemeral=True)
+    await interaction.response.send_message('Turning off...', ephemeral=True)
+    await client.close()
