4b7d14aaa4129db42f3eac612a8404c8cf918a62
tot0p <thomas.lemaitre.78420@gmail.com>
2023-04-14 09:18:52 +0200

    add role update

--- commands/slash/role/add.py
+++ commands/slash/role/add.py
@@ -4,12 +4,15 @@ from discord import app_commands
 
 @group.command()
 @app_commands.describe(name='The name of role to add')
-async def add(interaction: discord.Interaction, name: str):
+@app_commands.describe(user='The user to add the role to')
+async def add(interaction: discord.Interaction, name: str , user: discord.Member = None):
     """Add a role"""
     guild = interaction.guild
+    if user is None:
+        user = interaction.user
     role = discord.utils.get(guild.roles, name=name)
     if role is None:
         return await interaction.response.send_message('That role does not exist.', ephemeral=True)
-    await interaction.user.add_roles(role)
+    await user.add_roles(role)
     await interaction.response.send_message(f'Added the {role.name} role to you.', ephemeral=True)
 
---
+++ dataType/fifo.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+
+class fifo:
+
+
+    def __init__(self) -> None:
+        pass
