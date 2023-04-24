9c58a45219329c1829401f53b221a8a3f5a13cf2
tot0p <thomas.lemaitre.78420@gmail.com>
2023-04-24 09:17:48 +0200

    add lock to the history

--- historique.py
+++ historique.py
@@ -17,8 +17,7 @@ from uuid import uuid4
 
 
 class AccessHistorique:
-    def __init__(self,historique):
-        self.historique = historique
+    def __init__(self):
         self.queue = Queue()
 
     def lock(self):
@@ -41,31 +40,46 @@ class AccessHistorique:
 class Historique:
     def __init__(self):
         self.all_historique = {} 
+        self.access = AccessHistorique()
         self.load()
 
     def show(self,user_id) -> str:
+        uuid = self.access.lock()
+        self.access.wait(uuid)
         if str(user_id) in self.all_historique:
+            self.access.unlock(uuid)
             return str(self.all_historique[str(user_id)])
         else:
+            self.access.unlock(uuid)
             return "You don't have any command in your historique"
     
     def add(self,user_id,command):
+        uuid = self.access.lock()
+        self.access.wait(uuid)
         if str(user_id) in self.all_historique:
             self.all_historique[str(user_id)].append(TimeDate() + " " + command)
         else:
             self.all_historique[str(user_id)] = ListChained(TimeDate() + " " +command)
         self.save()
+        self.access.unlock(uuid)
 
     def clear(self,user_id) -> str:
+        uuid = self.access.lock()
+        self.access.wait(uuid)
         if user_id in self.all_historique:
             del self.all_historique[str(user_id)]
             self.save()
+        self.access.unlock(uuid)
         return "Your historique is clear"
 
     def last(self,user_id):
+        uuid = self.access.lock()
+        self.access.wait(uuid)
         if user_id in self.all_historique:
+            self.access.unlock(uuid)
             return self.all_historique[str(user_id)].firstNode.data
         else:
+            self.access.unlock(uuid)
             return "You don't have any command in your historique"
 
 
