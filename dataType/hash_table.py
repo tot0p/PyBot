f6a56f60ef525477166819be762e322accb5a802
tot0p <thomas.lemaitre.78420@gmail.com>
2023-04-24 10:12:42 +0200

    add hashtable

--- dataType/hash_table.py
+++ dataType/hash_table.py
@@ -1,4 +1,22 @@
 #-*- coding: utf-8 -*-
 
+class Hashtable:
+    def __init__(self, elements):
+        self.bucket_size = len(elements)
+        self.buckets = [[] for i in range(self.bucket_size)]
+        self._assign_buckets(elements)
 
+    def _assign_buckets(self, elements):
+        for key, value in elements:
+            hashed_value = hash(key)
+            index = hashed_value % self.bucket_size
+            self.buckets[index].append((key, value))
 
+    def get_value(self, input_key):
+        hashed_value = hash(input_key)
+        index = hashed_value % self.bucket_size
+        bucket = self.buckets[index]
+        for key, value in bucket:
+            if key == input_key:
+                return(value)
+        return None
