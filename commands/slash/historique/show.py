0143a4cbee5080055291990036c86b54f883e33a
lemaitre <Thomas.lemaitre.78420@gmail.com>
2023-05-13 16:05:01 +0200

    update historique

--- commands/slash/historique/show.py
+++ commands/slash/historique/show.py
@@ -1,7 +1,16 @@
 #-*- coding: utf-8 -*-
 import discord
+from widget.pagination import PaginationView
+
 
 @group.command(name='show', description="Show the historique")
 async def show(interaction: discord.Interaction):
     """Show the historique"""
-    await interaction.response.send_message(client.historique.show(interaction.user.id), ephemeral=True)
+    txt = client.historique.show(interaction.user.id)
+    embeds = []
+
+    hist = txt.split('\n')
+    for i in range(0, len(hist), 10):
+        embeds.append(discord.Embed(title=f"Page {i // 10 +1}", description='\n'.join(hist[i:i+10])))
+    view = PaginationView(embeds)
+    await interaction.response.send_message(embed=embeds[0], view=view)
